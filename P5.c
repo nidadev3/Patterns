3681a0226b9a0623441a8804311a1cad311ed026
Nida Arshad <nidaarshad120@gmail.com>
2026-02-26 13:16:42 -0800

    Increasing Characters

--- P5.c
+++ P5.c
@@ -1,12 +1,25 @@
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main (){
+//     int n=3;
+//     int num=1;
+//     for(int i =0; i<n; i++){
+//         for(int j=0; j<n; j++){
+//             printf("%d ", num);
+//             num++;
+//         }
+//         printf("\n");
+//     }
+// }
 #include<stdio.h>
 #include<stdlib.h>
 int main (){
     int n=3;
-    int num=1;
+ char ch='A';
     for(int i =0; i<n; i++){
         for(int j=0; j<n; j++){
-            printf("%d ", num);
-            num++;
+            printf( "%c ", ch);
+            ch++;
         }
         printf("\n");
     }
