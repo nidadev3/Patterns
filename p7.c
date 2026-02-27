4e3110a83bd1de6e4745bdd2263abcf4a5b19f80
Nida Arshad <nidaarshad120@gmail.com>
2026-02-27 08:18:51 -0800

    Some Changes in the existing patterns

--- p7.c
+++ p7.c
@@ -2,9 +2,11 @@
 #include<stdlib.h>
 int main(){
     int n=4;
+    int num=1;
     for(int i=n;i>0;i--){
         for(int j=i+1;j>0;j--){
-            printf("%d ",j);
+            printf("%d ",num);
+            num++;
         }
         printf("\n");
     }
